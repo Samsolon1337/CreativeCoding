cfd736fa835d46c0b651fca9abf5012fad0a1e58
Samsolon1337 <leon.steinbach01@gmail.com>
2022-07-23 11:00:34 +0200

    a new idea

--- myscript.js
+++ myscript.js
@@ -1,35 +1,15 @@
-//variables
-var info = document.getElementById("info");
-document.get
-var t = document.getElementById("test");
-var box = document.getElementById("container");
+void setup()
+{
 
-var bbox = box.getBoundingClientRect();
-
-var creating = true;
-// Keyboard Events
-
-
-//Mouse Events
-document.addEventListener('click', function(e) {
-    e= e || window.event;
-    var target = e.target;
-    creating = false;
-}, false);
-addEventListener("mousemove", getMousePositon, false);
-
-
-function getMousePositon(p){
-    if(creating){
-    info.innerText = `X: ${p.clientX} Y: ${p.clientY}\n BoxX: ${bbox.x} BoxY: ${bbox.y}\n BoxW: ${bbox.width} BoxH: ${bbox.height}`;
-    box.style.width = `${p.clientX - bbox.x -8}px`;
-    box.style.height = `${p.clientY - bbox.y -8}px`;
-}
-
+    createCanvas(400,400);
+    background()
 
 }
 
-function test(e){
-    t.innerHTML = `hey${e}`;
 
+void draw()
+{
+
+    line()
 }
+
