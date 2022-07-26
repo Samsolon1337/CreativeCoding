52561917db31aecc6e0c17e3ea239299b44470b7
Samsolon1337 <leon.steinbach01@gmail.com>
2022-07-26 02:11:33 +0200

    Update myscript.js

--- script/myscript.js
+++ script/myscript.js
@@ -1,20 +1,27 @@
+//TODO: Convert segments into squares
+//      Overlap them
+//      Give them color
+//      convert movement on y and y also to z 
+
 //      My classes
 class vec2{
     constructor(_x=0,_y=0){
         this.x = _x;
         this.y = _y;
+        this.isnormalized 
     }
     length(){
         return Math.sqrt((this.x*this.x)+(this.y*this.y));// Satz des Petagruas
     }
-    lengths(_v){
-        let a =_v;
-        return Math.sqrt(((a.x*a.x)-(this.x*this.x))+((a.y*a.y)-(this.y*this.y)));// Satz des Petagruas
-    }
+
     normalize(){
         let scale = this.length();
         this.x /= scale;
         this.y /= scale;
+        this.isnormalized = true;
+    }
+    get is_normalized(){
+        return this.isnormalized;
     }
     // Add
     add(_newvec = 0, _newvec2 = 0){
@@ -25,6 +32,9 @@ class vec2{
         if(_newvec instanceof vec2){
             return new vec2((this.x + _newvec.x),(this.y + _newvec.y));
         }
+        if(typeof(_newvec) === "number"){
+            return new vec2((this.x + _newvec),(this.y + _newvec));
+         }
     }
     // Subtract
     sub(_newvec = 0, _newvec2 = 0){
@@ -35,6 +45,9 @@ class vec2{
         if(_newvec instanceof vec2){
             return new vec2((this.x - _newvec.x),(this.y - _newvec.y));
         }
+        if(typeof(_newvec) === "number"){
+            return new vec2((this.x - _newvec),(this.y - _newvec));
+         }
     }
     // Multiply
     dot(_newvec = 0, _newvec2 = 0){
@@ -45,6 +58,9 @@ class vec2{
         if(_newvec instanceof vec2){
             return new vec2((this.x * _newvec.x),(this.y * _newvec.y));
         }
+        if(typeof(_newvec) === "number"){
+            return new vec2((this.x * _newvec),(this.y * _newvec));
+         }
     }
     // Divide
     div(_newvec = 0, _newvec2 = 0){
@@ -54,6 +70,9 @@ class vec2{
         if(_newvec instanceof vec2){
             return new vec2((this.x / _newvec.x),(this.y / _newvec.y));
         }
+        if(typeof(_newvec) === "number"){
+            return new vec2((this.x / _newvec),(this.y / _newvec));
+         }
     }
     
 }
@@ -72,17 +91,15 @@ class Segment{
         if (_parent instanceof Segment){ //Following Segments
             this.parent = _parent;     
             this.name = this.parent.name +1;
-            this.a.x = this.parent.b.x;
-            this.a.y = this.parent.b.y;
-            this.angle = -(Math.PI / 180)*_angle + this.parent.angle; // Converts the angle to degrees
+            this.a = this.parent.b;
+            this.angle = (Math.PI / 180)*_angle + this.parent.angle; // Converts the angle to degrees
             this.len = _len;
 
                
         }
         else {//First Segment
             this.name = 0;
-            this.a.x = _a.x;
-            this.a.y = _a.y;
+            this.a = _a;
             //initialize origin
             this.origin = this.a;
 
@@ -90,39 +107,40 @@ class Segment{
             this.len = _len;
             this.parent = _parent;
         }
-        this.calcBfromAngle();
+        this.calc_b();
+        console.log(this);
 
 
     }
     //      Methods
-    calcBfromAngle(){
+    calc_b(){
 
         this.angChange.x = this.len* Math.cos(this.angle);
         this.angChange.y = this.len* Math.sin(this.angle);
-        this.b.x = this.a.x + this.angChange.x;
-        this.b.y = this.a.y + this.angChange.y;
-
+        this.b = this.a.add(this.angChange);
+        
     }
 
     rotate(){
-        this.angle += Math.sin(this.angle) * 1;
-        this.calcBfromAngle();
+        if(this.angle>45)this.angle += Math.sin(this.angle) * 1.4;
+        if(this.angle<135)this.angle += Math.sin(Math.PI*700) * 1.4;
+        this.calc_b();
     }
 
     update(){
         if(this.parent != null){
-            this.parent.calcBfromAngle();
-            this.a.x = this.parent.b.x;
-            this.a.y = this.parent.b.y;
-            //this.angle = this.parent.angle;
+            this.parent.calc_b();
+            this.a = this.parent.b;
+            this.angle = this.parent.angle * this.angle;
 
         }
-        this.calcBfromAngle();
+        this.calc_b();
     }
 
     
     drawline(ctx){ 
-        this.calcBfromAngle();
+        this.calc_b();
+        //ctx.strokeStyle = `rgba(${this.angle*100},${this.angle*100},${this.angle*100},1)`;
         ctx.beginPath();
         ctx.moveTo(this.a.x,this.a.y);
         ctx.lineTo(this.b.x,this.b.y);
@@ -130,62 +148,50 @@ class Segment{
 
     }
 
-    reactToMouse(ctx){
+    react_to_mouse(ctx){
         ctx.fillRect(this.origin.x,this.origin.y,5,5);
         if(this.parent == null){
-            let vec = new vec2(), placeholder, limit = 60;
+            let dir, placeholder = new vec2(), limit = 60;
 
-            vec.x = this.a.x-mouse.x;
-            vec.y= this.a.y-mouse.y;
+            dir = this.a.sub(mouse);//substract multiple vectors to create directional vector
             
             if(this.a.x > mouse.x - limit && this.a.x < mouse.x + limit &&
             this.a.y > mouse.y - limit && this.a.y < mouse.y + limit){
                 ctx.strokeStyle = "red";
-                vec.normalize();
+                dir.normalize();
                // console.log(`B:${this.a.y}, B:${this.a.x}, C:${this.origin.x} B:${this.origin.y}`);
                 //i = this.calcDistance(this.a.x,this.a.y,this.origin.x,this.origin.y);
                 //if( i <= limit){
-                placeholder = this.origin;
+        
+                console.log(this.a.length()-this.origin.length());
 
-                if(this.a.lengths(placeholder) < limit){
+                if(this.origin.length()-this.a.length() <= limit){
+                    this.a.x = this.a.x + (limit*dir.x);//move point along circle using directional vetor
+                    this.a.y = this.a.y + (limit*dir.y);
 
-
+                }
+                // FIX: IS NEVER CALLED!!!!!
+                   this.a.x = 600;
+                   this.a.y = 600;
                 
-                    this.a.x = placeholder.x + (limit *vec.x);
-                    this.a.y = placeholder.y + (limit *vec.y);
-                }
-
-                else{
-                   this.a.x = this.origin.x;
-                   this.a.y = this.origin.y;
-                }
                 
             }
-            
+            // IDEA: USE DIR AS ANGLE FOR FIRST SEGMENT; TO CHECK FOWARD KINEMATIC
+            ctx.save();
             ctx.beginPath();
+            ctx.lineWidth= 3;
             ctx.arc(mouse.x, mouse.y, limit, 0,  2 * Math.PI);
             ctx.moveTo(this.a.x,this.a.y);
-            ctx.lineTo(this.a.x+vec.x,this.a.y+vec.y);
+            ctx.lineTo(this.a.x+dir.x,this.a.y+dir.y);
             ctx.stroke();
-    
+            ctx.restore();
         }
     }   
-    
-    //      Setter 
-    set Angle(_angle){
-        this.angle = (-(Math.PI / 180)*_angle); // Converts the angle to degrees
-    } 
-    set A(_a){
-        this.a = _a;
-    } 
-    set Length(_len){
-        this.len = _len;
-    } 
 }
 
 //       Event Listenrer
 addEventListener("load", setup);
-addEventListener("resize", updateBox);
+addEventListener("resize", update_box);
 document.onmousemove = function(e){ // get mouse position
     mouse.x = e.clientX-cRect.left;
     mouse.y = e.clientY-cRect.top;
@@ -203,7 +209,7 @@ const docSize  = new vec2(canvas.width, canvas.height);//easier Access to width 
 let cRect = canvas.getBoundingClientRect();
 
 //Segment Setup
-var amount = 8;
+var amount = 3;
 var segments = createSegments();
 // Mouse Position
 var mouse = new vec2(), prev = new vec2();
@@ -213,6 +219,18 @@ var mouse = new vec2(), prev = new vec2();
 
 
 // My Functions
+
+function createSegments(){
+    let vec = new vec2();
+    vec = docSize.div(2);
+    let segs = [];
+    let size = 100;
+    let angle = 31.415;
+    for(let i = 0; i< amount; i++){
+        segs.push(new Segment(vec,angle,size,segs[i-1]));
+    }
+    return segs;
+}
 
 function setup() {
     if (!canvas.getContext) {
@@ -227,19 +245,6 @@ function setup() {
 
 }
 
-function createSegments(){
-    let vec = new vec2();
-    vec = docSize.div(new vec2(2,2));
-
-    let segs = [];
-    let size = 100;
-    let angle = 10;
-    for(let i = 0; i<= amount; i++){
-        segs.push(new Segment(vec,angle,(size),segs[i-1]));
-    }
-    return segs;
-}
-
 function draw(){
 
     let ctx = setup();
@@ -249,10 +254,10 @@ function draw(){
 
 
 
-    for(let i = 0; i<= amount; i++){
+    for(let i = 0; i < amount; i++){
     segments[i].drawline(ctx);
     segments[i].update();
-    segments[0].reactToMouse(ctx);
+    segments[i].react_to_mouse(ctx);
     segments[i].rotate();
     }
    // console.log(segments);
@@ -272,7 +277,7 @@ function draw(){
 }
 
 
-function updateBox(){
+function update_box(){
     cRect = canvas.getBoundingClientRect(); 
 }
 
